c01a22661a527a48f22c0349438db5b20dfa274c
ErckLima <erickfalubay@gmail.com>
2022-09-19 19:36:29 -0300

    Criação dos botoes personalizados
    Site utilizado:
    
    https://edrodrigues.com.br/blog/20-incriveis-botoes-animados-de-css-puro/

---
+++ ScriptGabi.js
@@ -0,0 +1,8 @@
+function Entrar(){
+    let pagina = document.getElementById("Tudo")
+    pagina.style.backgroundColor = "rgb(223, 70, 96)"
+}
+function Sair(){
+    let pagina = document.getElementById("Tudo")
+    pagina.style.backgroundColor = "rgb(228, 121, 139)"
+}
